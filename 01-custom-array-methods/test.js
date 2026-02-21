7cb149c882cf09dda5689824d06af09809dbd7fe
khalil-menjli <menjlikhalyl@gmail.com>
2026-02-21 23:26:01 +0100

    feat: add remaining tests for 01-custom-array-methods

--- 01-custom-array-methods/test.js
+++ 01-custom-array-methods/test.js
@@ -27,3 +27,30 @@ test("mySome should return the same result as native map", () => {
 test("myEvery should return the same result as native map", () => {
   expect(arr.myEvery((x) => x > 33)).toEqual(arr.every((x) => x > 33));
 });
+
+test("myForEach should produce the same side effects as native forEach", () => {
+  const nativeResult = [];
+  const myResult = [];
+  arr.forEach((x) => nativeResult.push(x * 2));
+  arr.myForEach((x) => myResult.push(x * 2));
+  expect(myResult).toEqual(nativeResult);
+});
+
+test("myFind should return the same result as native find", () => {
+  expect(arr.myFind((x) => x > 4)).toEqual(arr.find((x) => x > 4));
+});
+
+test("myFindIndex should return the same result as native findIndex", () => {
+  expect(arr.myFindIndex((x) => x > 4)).toEqual(arr.findIndex((x) => x > 4));
+});
+
+test("myFlat should return the same result as native flat", () => {
+  const nested = [1, [2, 3], [4, [5, 6]]];
+  expect(nested.myFlat(2)).toEqual(nested.flat(2));
+});
+
+test("myFlatMap should return the same result as native flatMap", () => {
+  expect(arr.myFlatMap((x) => [x, x * 2])).toEqual(
+    arr.flatMap((x) => [x, x * 2]),
+  );
+});
