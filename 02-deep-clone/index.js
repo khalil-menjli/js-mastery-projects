eb816a3066034bf6a8196a18e14d1e5197abc9c7
khalil-menjli <menjlikhalyl@gmail.com>
2026-02-23 23:57:04 +0100

    feat: complete 02-deep-clone implementation

---
+++ 02-deep-clone/index.js
@@ -0,0 +1,34 @@
+function deepClone(obj, seen = new WeakMap()) {
+  if (typeof obj !== "object" || obj === null) return obj;
+  if (seen.has(obj)) return seen.get(obj);
+  let copy;
+
+  if (Array.isArray(obj)) {
+    copy = [];
+  } else if (obj instanceof Map) {
+    copy = new Map();
+    seen.set(obj, copy);
+    obj.forEach((value, key) => {
+      copy.set(deepClone(key,seen), deepClone(value, seen));
+    });
+    return copy;
+  } else if (obj instanceof Set) {
+    copy = new Set();
+    seen.set(obj, copy);
+    obj.forEach((value) => {
+      copy.add(deepClone(value, seen));
+    });
+    return copy;
+  } else if (obj instanceof Date) {
+    copy = new Date(obj);
+  } else if (obj instanceof RegExp) {
+    copy = new RegExp(obj);
+  } else {
+    copy = Object.create(Object.getPrototypeOf(obj));
+  }
+  seen.set(obj, copy);
+  for (let key in obj) {
+    copy[key] = deepClone(obj[key], seen);
+  }
+  return copy;
+}
